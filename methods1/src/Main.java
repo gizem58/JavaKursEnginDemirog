7e3a15f3da58c3cc4b6d2c172085d7c12b1fd157
gizemcaymaz <gizem0799@gmail.com>
2022-10-13 16:23:29 +0200

    Java Dersler

---
+++ methods1/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+        sayiBulmaca();
+        sayiBulmaca();
+        sayiBulmaca();
+        sayiBulmaca();
+    }
+    public static void sayiBulmaca() {
+        int[] sayilar = new int [] {1,2,5,7,9,0};
+        int aranacak = 5;
+        boolean GibtEsDieZahl = false;
+
+        for (int sayi : sayilar){
+            if(sayi==aranacak){
+                GibtEsDieZahl=true;
+                break;
+            }
+        }
+
+        if(GibtEsDieZahl)   {            //Ist das Gleiche wie gibtesdieZahl==true
+            mesajVer("Sayi mevcuttur:" +aranacak);
+        }else{
+            mesajVer("Sayi mevcutturdegildir:" +aranacak);
+        }
+    }
+
+    public static void mesajVer(String mesaj){
+        System.out.println(mesaj);
+    }
+
+}
+//
---
+++ sayiBulmaTest/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] sayilar = new int [] {1,2,5,7,9,0};
+        int aranacak = 5;                               //Soru 5 icinde varmi, yokmu?
+        boolean GibtEsDieZahl = false;
+
+        for (int sayi : sayilar){           //Nur eine Zahl von allen und frage ob die aranack mit der Zahl identisch ist
+            if(sayi==aranacak){
+                GibtEsDieZahl=true;
+                break;
+
+            }
+            }
+
+        if(GibtEsDieZahl)   {            //Ist das Gleiche wie gibtesdieZahl==true
+            System.out.println("Es gibt diese Zahl");
+        }else{
+            System.out.println("Es gibt diese Zahl nicht");
+        }
+
+
+    }
+}
