5472ac00bbe3506805f383545a6e4f6090860f32
gizemcaymaz <gizem0799@gmail.com>
2022-11-08 16:27:29 +0100

    Java Dersler

---
+++ inheritanceDemo/src/KrediUI.java
@@ -0,0 +1,9 @@
+public class KrediUI {                  //KrediUserinterface
+    public void KrediHesapla( BaseKrediManager baseKrediManager){
+        baseKrediManager.Hesapla();
+
+
+    }
+
+
+}
---
+++ inheritanceDemo/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	 KrediUI krediUI = new KrediUI();
+     krediUI.KrediHesapla(new OgretmenKrediManager()); //oder asker oder tarim... //das alles nennt sich Polymophie
+    }
+}
